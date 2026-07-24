1c95bf5f2d3f34223ec03f306eff24fe3c42d0d4
jesuscamilo901 <jesuscamilo901@gmail.com>
2026-07-24 11:15:40 -0700

    Menus modal

--- src/menu/menu.ts
+++ src/menu/menu.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './menu.css',
 })
 export class Menu {
+  isModalOpen: boolean = false;
+  selectedImage: string = '';
+  selectedTitle: string = ''; // NUEVO
+  selectedDesc: string = '';  // NUEVO
 
+  // Actualizamos la función para recibir 3 parámetros
+  openModal(imageUrl: string, title: string, desc: string) {
+    this.selectedImage = imageUrl;
+    this.selectedTitle = title;
+    this.selectedDesc = desc;
+    this.isModalOpen = true;
+  }
+
+  // Limpiamos las variables al cerrar
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedImage = '';
+    this.selectedTitle = '';
+    this.selectedDesc = '';
+  }
 }
